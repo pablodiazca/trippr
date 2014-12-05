ae315ff7202f3256c747d5438786dbbb941c0986
Pablo Diaz <pablo@pdiaz.ca>
2014-12-04 16:31:20 -0800

    remove needless #s

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,3 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-
-
 //$(function() {
 //  $('a[href*=#]:not([href=#])').click(function() {
 //    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
